2a5225f0b8fd1329fd46bf1c86d2cc07fc8a8de4
Seunghyo Lee <bingwer@naver.com>
2020-05-01 23:19:26 +0900

    day 9 homework

--- Day7/day7.js
+++ Day7/day7.js
@@ -3,18 +3,17 @@ const toDoForm = document.querySelector(".js-toDoForm"),
   toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
-const toDos = [];
-
-function filterFn(toDo) {
-  return toDo.id === 1;
-}
+let toDos = [];
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(filterFn);
-  console.log(cleanToDos);
+  const cleanToDos = toDos.filter(function (toDo) {
+    return toDo.id !== parseInt(li.id);
+  });
+  toDos = cleanToDos;
+  saveToDos();
 }
 
 function saveToDos() {
@@ -26,7 +25,7 @@ function paintToDo(text) {
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerHTML = "ⓧ";
+  delBtn.innerHTML = "❌";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(span);
@@ -35,7 +34,7 @@ function paintToDo(text) {
   toDoList.appendChild(li);
   const toDoObj = {
     text: text,
-    id: newId
+    id: newId,
   };
   toDos.push(toDoObj);
   saveToDos();
@@ -52,7 +51,7 @@ function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
-    parsedToDos.forEach(function(toDo) {
+    parsedToDos.forEach(function (toDo) {
       paintToDo(toDo.text);
     });
   }
---
+++ Day9_homework/index.js
@@ -0,0 +1,134 @@
+const numbers = Array.from(document.querySelectorAll(".number"));
+const operators = Array.from(document.querySelectorAll(".operator"));
+const result = document.querySelector(".result");
+const reset = document.querySelector(".reset");
+const equal = document.querySelector(".equal");
+const back = document.querySelector(".back");
+
+let firstNum = "";
+let secondNum = "";
+let selectedOperator = "";
+let firstFlag = false;
+let secondFlag = false;
+let clickedNum;
+
+function resetAll() {
+  firstNum = "";
+  secondNum = "";
+  firstFlag = false;
+  secondFlag = false;
+  result.innerText = "0";
+}
+
+function operate() {
+  const firstOpNum = parseFloat(firstNum);
+  const secOpNum = parseFloat(secondNum);
+  if (selectedOperator === "+") {
+    return firstOpNum + secOpNum;
+  } else if (selectedOperator === "-") {
+    return firstOpNum - secOpNum;
+  } else if (selectedOperator === "*") {
+    return firstOpNum * secOpNum;
+  } else if (selectedOperator === "/") {
+    if (Number.isInteger(firstOpNum / secondNum) === true) {
+      return firstOpNum / secondNum;
+    } else {
+      return (firstOpNum / secOpNum).toFixed(5);
+    }
+  } else {
+    return;
+  }
+}
+
+function calculate() {
+  const oprationValue = operate();
+  result.innerText = oprationValue;
+  firstNum = oprationValue;
+  secondNum = "";
+  secondFlag = false;
+}
+
+function equalCalculate() {
+  const oprationValue = operate();
+  result.innerText = oprationValue;
+  firstNum = oprationValue;
+  console.log(firstNum, secondNum, selectedOperator);
+}
+
+function numberClick(event) {
+  clickedNum = event.target.innerText;
+  if (firstFlag !== true) {
+    firstNum = firstNum + clickedNum;
+    result.innerText = firstNum;
+  } else {
+    secondNum = secondNum + clickedNum;
+    result.innerText = secondNum;
+    secondFlag = true;
+  }
+}
+
+function operatorClick(event) {
+  const clickedOperator = event.target.innerText;
+  if (firstFlag !== true) {
+    firstFlag = true;
+  }
+  if (firstFlag === true && secondFlag === true) {
+    calculate();
+  }
+  selectedOperator = clickedOperator;
+}
+
+function resetClick() {
+  resetAll();
+}
+
+function equalClick() {
+  if (firstFlag === true && secondFlag === true) {
+    equalCalculate();
+  }
+}
+
+function backClick() {
+  if (firstFlag !== true) {
+    if (firstNum !== "" && firstNum.toString().length !== 0) {
+      const numLength = firstNum.toString().length;
+      firstNum = String(firstNum).substring(0, numLength - 1);
+      result.innerText = firstNum;
+    }
+    if (firstNum.toString().length === 0) {
+      firstNum = "";
+      result.innerText = "0";
+    }
+  } else {
+    if (secondNum !== "" && secondNum.toString().length !== 0) {
+      const numLength = secondNum.toString().length;
+      secondNum = String(secondNum).substring(0, numLength - 1);
+      result.innerText = secondNum;
+    }
+    if (secondNum.toString().length === 0) {
+      secondNum = "";
+      result.innerText = firstNum;
+      secondFlag = false;
+    }
+  }
+}
+
+numbers.forEach(function (number) {
+  number.addEventListener("click", numberClick);
+});
+
+operators.forEach(function (oprator) {
+  oprator.addEventListener("click", operatorClick);
+});
+
+reset.addEventListener("click", resetClick);
+
+equal.addEventListener("click", equalClick);
+
+back.addEventListener("click", backClick);
+
+function init() {
+  result.innerText = "0";
+}
+
+init();
